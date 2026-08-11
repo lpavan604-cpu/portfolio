5098c0c307e1e1ff0411b85b4a288d878853d8c4
Pavan L <lpavan604@gmial.com>
2026-08-11 16:42:18 +0530

    initial commit: add portfolio files

---
+++ script.js
@@ -0,0 +1,13 @@
+const links=document.querySelectorAll("nav a");
+
+links.forEach(link=>{
+
+link.addEventListener("click",function(){
+
+links.forEach(l=>l.classList.remove("active"));
+
+this.classList.add("active");
+
+});
+
+});
